a5e5eeed11090f3af3f4f786823521c3f72cc812
jeffxhansen <jeffxhansen@gmail.com>
2022-03-24 09:47:43 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ npm run build
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-#git add -A
-#git commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 git push -f git@github.com:jeffxhansen/scrip-memorize-vue.git master:gh-pages
 cd -
